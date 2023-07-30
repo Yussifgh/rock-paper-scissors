78a242b70cc6094893332880ac71b9254426eae7
Yussifgh <130477751+Yussifgh@users.noreply.github.com>
2023-07-30 18:05:10 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-// JavaScript
+let playerScore = 0;
+let computerScore = 0;
+
 function play(playerChoice) {
+  const choices = ['rock', 'paper', 'scissors'];
+  const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  const result = playRound(playerChoice, computerChoice);
+  document.getElementById('results').innerText = result;
+}
+
+function updateScores() {
+  document.getElementById('player-score').innerText = `Player: ${playerScore}`;
+  document.getElementById('computer-score').innerText = `Computer: ${computerScore}`;
+}
+
+function playRound(playerChoice, computerChoice) {
+  if (!['rock', 'paper', 'scissors'].includes(playerChoice)) {
+    return "Invalid choice. Please choose 'rock', 'paper', or 'scissors'.";
+  }
+
+  let result = '';
+
+  if (playerChoice === computerChoice) {
+    result = "It's a tie!";
+  } else if (
+    (playerChoice === 'rock' && computerChoice === 'scissors') ||
+    (playerChoice === 'paper' && computerChoice === 'rock') ||
+    (playerChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    playerScore++;
+    result = `You win! You chose ${playerChoice}, computer chose ${computerChoice}.`;
+  } else {
+    computerScore++;
+    result = `You lose! You chose ${playerChoice}, computer chose ${computerChoice}.`;
+  }
+  updateScores();
+
+  return result;
+}
+
+function game() {
+  playerScore = 0;
+  computerScore = 0;
+
+  for (let x = 0; x < 5; x++) {
     const choices = ['rock', 'paper', 'scissors'];
+    const playerChoice = choices[Math.floor(Math.random() * choices.length)];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
     const result = playRound(playerChoice, computerChoice);
-    document.getElementById('results').innerText = result;
+    document.getElementById('results').innerText += result + "\n";
   }
-  
-  function playRound(playerChoice, computerChoice) {
-    if (!['rock', 'paper', 'scissors'].includes(playerChoice)) {
-      return "Invalid choice. Please choose 'rock', 'paper', or 'scissors'.";
-    }
-    if (playerChoice === computerChoice) {
-      return "It's a tie!";
-    } else if (
-      (playerChoice === 'rock' && computerChoice === 'scissors') ||
-      (playerChoice === 'paper' && computerChoice === 'rock') ||
-      (playerChoice === 'scissors' && computerChoice === 'paper')
-    ) {
-      return `You win! You chose ${playerChoice}, computer chose ${computerChoice}.`;
-    } else {
-      return `You lose! You chose ${playerChoice}, computer chose ${computerChoice}.`;
-    }
-  
-  
-  function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-  
-    for (let x = 0; x < 5; x++) {
-     const choices = ['rock', 'paper', 'scissors'];
-      const playerChoice = choices;
-      const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-      const result = playRound(playerChoice, computerChoice);
-      document.getElementById('results').innerHTML = result;
-  
-      if (result.includes('win')) {
-        playerScore++;
-      } else if (result.includes('lose')) {
-        computerScore++;
-      }
-    }
-  
-    if (playerScore > computerScore) {
-      document.getElementById('results').innerHTML = "You have won!";
-    } else if (playerScore < computerScore) {
-      document.getElementById('results').innerHTML = "Computer won!";
-    } else {
-      document.getElementById('results').innerHTML = "It's a tie!";
-    }
+  if (playerScore > computerScore) {
+    document.getElementById('results').innerText += "You have won the game!";
+  } else if (playerScore < computerScore) {
+    document.getElementById('results').innerText += "Computer won the game!";
+  } else {
+    document.getElementById('results').innerText += "It's a tie!";
   }
-  }
-  
-  
+}
